ae2a84d83f090a2e3d2cdc84400451511c41783f
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-05-26 20:47:14 +0300

    done

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,38 @@
+import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import ErrorPage from "./pages/ErrorPage";
+import { Children } from "react";
 
 const routes = [
   {
     path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
-  }
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+         {
+            path: "/",
+            element: <Home />,
+            children: [
+              
+        {
+            path: "/profile/:id",
+            element: <UserProfile />
+        }
+            ]
+        }, 
+        {
+            path: "/about",
+            element: <About />
+        },
+        {
+            path: "/login",
+            element: <Login />
+        }
+    ]
+}
 ];
 
 export default routes;
